fa735a2ad28eb6a1db4d84418e6e941d833297ab
Herman Gouw <hermangouw@gmail.com>
2018-11-26 12:31:25 +1100

    Used Upper-Case package

--- Listen.js
+++ Listen.js
@@ -1,9 +1,10 @@
 var http = require('http');
 var url = require('url');
+var uc = require('upper-case');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('Hello there!<br/>');
+    res.write(uc('Hello there!<br/>'));
     var q = url.parse(req.url, true).query;
     var txt = q.year + " " + q.month;
     res.write(txt);
