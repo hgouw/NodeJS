f6b6283869a5aac23e694f9c51bbc1cd2b5fe3b7
Herman Gouw <hermangouw@gmail.com>
2018-11-26 12:04:42 +1100

    Updated Listen.js

--- Listen.js
+++ Listen.js
@@ -1,6 +1,11 @@
 var http = require('http');
+var url = require('url');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello there!');
-}).listen(8080);
+    res.write('Hello there!<br/>');
+    var q = url.parse(req.url, true).query;
+    var txt = q.year + " " + q.month;
+    res.write(txt);
+    res.end();
+}).listen(4200);
