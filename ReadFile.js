84b2f36f1e1198b9cd7fb4dcf237ac3401db6bbb
Herman Gouw <hermangouw@gmail.com>
2019-01-07 15:52:59 +1100

    Added comments

--- ReadFile.js
+++ ReadFile.js
@@ -1,11 +1,11 @@
-// Blocking Code
+// Blocking Code (Sync Read)
 var fs = require("fs");
 var data = fs.readFileSync('input.txt');
 
 console.log(data.toString());
 console.log("Program Ended");
 
-//Non-Blocking Code
+//Non-Blocking Code (Async Read)
 fs.readFile('input.txt', function (err, data) {
     if (err) return console.error(err);
     console.log(data.toString());
